a07997fc8a42be39a62a3b9929bacedcf649a227
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-05-24 09:15:04 -0300

    add function checaCodigo() and the variable (codigo)

--- static/js/rastreamentoScript.js
+++ static/js/rastreamentoScript.js
@@ -1,11 +1,12 @@
-let user = 'carlos_eduardo2761@hotmail.com';
-let token = '83429b38f609e7ee3bc0fb8b51e8ce84be546289fdb2a40207b0f4bc0c0452ad'
-let codigo = 'QP073635129BR'
+let codigo = localStorage.getItem('codigo');
 
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-};
+document.addEventListener('DOMContentLoaded', function(event){
+    const codRastreamento = document.querySelector('h2');
+    codRastreamento.textContent = codigo;
+
+})
+
+
 
 
 // Funcões que criam elementos
@@ -34,23 +35,61 @@ function divStatus(status){
     return divElement
 }
 
-fetch(`https://api.linketrack.com/track/json?user=${user}&token=${token}&codigo=${codigo}`, requestOptions)
+
+// Configurações API
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+const user = 'carlos_eduardo2761@hotmail.com';
+const token = '83429b38f609e7ee3bc0fb8b51e8ce84be546289fdb2a40207b0f4bc0c0452ad';
+const apiLink = `https://api.linketrack.com/track/json?user=${user}&token=${token}&codigo=${codigo}`;
+
+// API
+fetch(apiLink, requestOptions)
+    .catch(err => console.log(err))
     .then(response => response.json())
     .then(dados => {
         const dat = dados.eventos;
+        const situacao = document.getElementById('situacaoPacote');
+        situacao.textContent = dat[0].status;
+        
         for(let i = 0; i < dados.eventos.length; i++){
             const dat = dados.eventos[i];
             const elemento = document.getElementById('informacoesPacotes');
             const liElement = document.createElement('li');
-            elemento.appendChild(liElement);
+            elemento.appendChild(liElement)
             liElement.appendChild(divDataHora(dat.data, dat.hora))
             liElement.appendChild(divLocal(dat.local))
             liElement.appendChild(divStatus(dat.status))
 
         }
-        
-
-        
-        
+           
     })
-    .catch(err => console.log(err))
+
+
+// Função para checar se o codigo é válido.
+
+function checaCodigo(){
+    const codigo = document.getElementById('inputCodigo').value.toUpperCase();
+    localStorage.setItem('codigo', codigo);
+
+    if(codigo.length == 13){
+
+        // API
+        fetch(apiLink, requestOptions)
+            .catch(error => {
+                alert('Código inválido')
+            })
+            .then(data => {
+                location.reload();      
+            })
+        
+    }
+
+    else{
+        alert('Código inválido')
+    }
+
+}
