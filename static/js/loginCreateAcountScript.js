bbd1db786d68c3a609c26f62c041a1f00360509e
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-06-17 00:25:40 -0300

    setting checaEmail and checaSenha functions

--- static/js/loginCreateAcountScript.js
+++ static/js/loginCreateAcountScript.js
@@ -12,48 +12,59 @@ function checaEmail(id){
       };
     
     let email = document.getElementById(id).value;
-    let access_key = '79a8fc5f62c19a60bfa99202aeb84991'
     let url = `https://api.apilayer.com/email_verification/check?email=${email}`
     
     fetch(url, requestOptions)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
-        })
-        .catch(error => {
-            if(error){
-                console.log('erro', error)
+            if(data.success == false){
+                let element = document.getElementById('errorMessageEmail');
+                element.innerHTML = 'Email inválido!'
+                return false
+            }
+            else{
+                return true;
             }
         })
 }
 
-function checaSenha(id, s2){
+function checaSenha(id){
     // Password's requeriments
     let upperCases = /[A-Z]/;
     let lowerCase = /[a-z]/;
     let numbers = /[0-9]/;
     let specialCharacters = /[!|@|#|$|%|^|&|*|(|)|-|_]/;
 
-    let senha = document.getElementById(id).value;
+    let senha = document.getElementById('senha').value;
+    let cSenha = document.getElementById('cSenha').value;
 
-    let element = document.getElementById(`errorMessage${s2}`);
+    let element = document.getElementById(`errorMessage${id}`);
     if(senha.length < 8){
-        element.innerHTML = 'A senha deve conter no mínimo 8 caracteres!';
+        element.innerHTML = 'A senha deve conter no mínimo 8 caracteres!'
+        return false
     }
     else if(upperCases.test(senha) == false){
-        element.innerHTML = 'A senha deve conter pelo menos 1 letra maiúscula!';
+        element.innerHTML = 'A senha deve conter pelo menos 1 letra maiúscula!'
+        return false
     }
     else if(lowerCase.test(senha) == false){
-        element.innerHTML = 'A senha precisa conter pelo menos 1 letra minúscula!';
+        element.innerHTML = 'A senha precisa conter pelo menos 1 letra minúscula!'
+        return false
     }
     else if(numbers.test(senha) == false){
-        element.innerHTML = 'A senha deve conter pelos menos 1 número!';
+        element.innerHTML = 'A senha deve conter pelos menos 1 número!'
+        return false
     }
     else if(specialCharacters.test(senha) == false){
-        element.innerHTML = 'A senha deve conter pelo menos 1 caractere especial!';
+        element.innerHTML = 'A senha deve conter pelo menos 1 caractere especial!'
+        return false
+    }
+    else if(senha != cSenha) {
+        element.innerHTML = 'As senhas não coincidem!'
+        return false
     }
     else{
-        element.innerHTML = '';
+        element.innerHTML = ''
         return true;
     }
    
